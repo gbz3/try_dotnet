44c0bebf932ec3809d7f9a2d960985c2f1d92973
gbz3 <1808483+gbz3@users.noreply.github.com>
2026-02-07 03:18:07 +0000

    Refactor database context and models: replace AppDbContext with PubsDbContext and update entity models for Authors, Titles, Publishers, Stores, Sales, and TitleAuthors.

--- BlazorApp02/ConsoleApp06/Program.cs
+++ BlazorApp02/ConsoleApp06/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.EntityFrameworkCore;
+
 Console.WriteLine("Hello, World!");
+
+var builder = new DbContextOptionsBuilder<ConsoleApp06.Data.PubsDbContext>();
+builder.UseSqlServer("Server=localhost,1433;Database=pubs;User Id=sa;Password=YourStrong@Passw0rd;TrustServerCertificate=True");
+
+using (var pubs = new ConsoleApp06.Data.PubsDbContext(builder.Options))
+{
+    var query1 = pubs.Authors
+        .Where(a => a.State == "CA");
+    var result1 = query1.ToList();
+
+    var query2 = pubs.Authors
+                    .Where(a => a.AuthorId == "172-32-1176")
+                    .Select(a => new { a.AuthorId, a.AuthorFirstName, a.AuthorLastName });
+    var result2 = query2.FirstOrDefault();
+
+    foreach(var author in result1)
+    {
+        Console.WriteLine($"{author.AuthorId}: {author.AuthorFirstName} {author.AuthorLastName} ({author.State})");
+    }
+
+    Console.WriteLine($"One: {result2?.AuthorId}: {result2?.AuthorFirstName} {result2?.AuthorLastName}");
+
+}
